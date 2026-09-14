1d22906f8730ebe80ae3d8d9269bc3b5ed79e52a
Aryan Patel <aryanpatel769894@gmail.com>
2026-09-14 17:14:40 +0530

    Delete assessment15 directory

--- assessment15/src/components/Topbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Topbar() {
-  return (
-    <div className="topbar">
-      <h3>Admin Panel</h3>
-    </div>
-  );
-}
--- assessment15/src/pages/Settings.jsx
+++
@@ -1,8 +0,0 @@
-export default function Settings() {
-  return (
-    <div>
-      <h1>Settings Page</h1>
-      <p>This is working correctly.</p>
-    </div>
-  );
-}
