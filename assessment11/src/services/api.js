49a1ec57fa874bb0d31b5be08789a1b1907d3d4f
Aryan Patel <aryanpatel769894@gmail.com>
2026-09-14 17:13:51 +0530

    Delete assessment11 directory

--- assessment11/src/services/api.js
+++
@@ -1,9 +0,0 @@
-export async function fetchProducts() {
-  const response = await fetch("https://fakestoreapi.com/products");
-
-  if (!response.ok) {
-    throw new Error("Failed to fetch products");
-  }
-
-  return await response.json();
-}
